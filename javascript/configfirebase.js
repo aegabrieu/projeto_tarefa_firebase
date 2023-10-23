269eeef1d74c14d989236fefecae177b7f7d1d80
Nogueira <Nogueiragabriel924@gmail.com>
2023-10-23 20:28:02 -0300

    criado projeto firebase

---
+++ javascript/configfirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFRDz3lqxLDqrrlcBMz2qtryMo8HMEEGM",
+  authDomain: "projetotarefa-fb.firebaseapp.com",
+  projectId: "projetotarefa-fb",
+  storageBucket: "projetotarefa-fb.appspot.com",
+  messagingSenderId: "880557945919",
+  appId: "1:880557945919:web:ff063e86b81babb2e3348a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
